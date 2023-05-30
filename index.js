a5be09ca0fd3c75526d360fb0b53fa1bd184e342
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-05-30 13:10:17 +0300

    done

---
+++ index.js
@@ -0,0 +1,12 @@
+function numberOfPairs(gloves) {
+    const hash = {}
+    for (let i = 0; i < gloves.length; i++) {
+        if (!hash[gloves[i]]) hash[gloves[i]] = 1;
+        else hash[gloves[i]] = hash[gloves[i]] + 1
+    }
+    let res = 0
+
+    Object.keys(hash).forEach(el => res += Math.floor(hash[el] / 2))
+
+    return res
+}
